d6b03b25ed001926fbaa2497b25fa83b253170e9
RyanADesigns <[ryan.stupey@gmail.com]>
2018-04-02 20:07:34 -0700

    made and styled navbar sfc component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,9 +4,9 @@ export default class NavBar extends Component {
     render() {
         return (
             <div className="nav_parent_div">
-              <div className="nav_Link_Li">EVENTS <i class="fas fa-calendar-alt"></i></div>
-              <div className="nav_Link_Li">GROUPS</div>
-              <div className="nav_Link_Li">FRIENDS</div>
+              <div className="nav_Link_Li"> <img src={require('../../assets/images/calendar.png')}/><h5>EVENTS</h5> </div>
+              <div className="nav_Link_Li"><img src={require('../../assets/images/add_group.png')}/><h5>GROUPS</h5></div>
+              <div className="nav_Link_Li"><img src={require('../../assets/images/friends-512.png')}/><h5>FRIENDS</h5></div>
           </div>
         );
     }
