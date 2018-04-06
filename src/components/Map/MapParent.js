71552023585fc8b1e752eee2690d8fc3fb4d3d7b
Tyler <millert2420@gmail.com>
2018-04-06 09:43:34 -0700

    Updating merge

--- src/components/Map/MapParent.js
+++ src/components/Map/MapParent.js
@@ -5,13 +5,16 @@ const MapWithAMarker = withScriptjs(withGoogleMap(props =>
     <GoogleMap
       defaultZoom={10}
       defaultCenter={{ lat: props.middlepoint[0], lng: props.middlepoint[1] }}
-  > {console.log("markers",props.markers)}
-    {props.markers.map(e=>{
-      if(e.checked){
-        return <Marker position={{lat:e.coordinates.latitude,lng:e.coordinates.longitude}}
-        onClick={(i)=>{props.displayInfoBox({i:i,name:e.name})}} />
-      }
-    })}
+  > {console.log("markers", props.markers)}
+    {props.markers ?
+        props.markers.map(e => {
+          if (e.checked) {
+            return <Marker position={{ lat: e.coordinates.latitude, lng: e.coordinates.longitude }}
+              onClick={(i) => { props.displayInfoBox({ i: i, name: e.name }) }} />
+          }
+        })
+      :
+      <div></div>}
 
     <Marker position={{ lat: props.middlepoint[0], lng: props.middlepoint[1] }}/>
     </GoogleMap>
