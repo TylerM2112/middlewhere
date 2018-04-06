d996c5b17eb60f3fb3068258cbffee27a8e8b7e3
Tyler <millert2420@gmail.com>
2018-04-06 11:41:02 -0700

    commiting to merge

--- src/components/views/FriendsView.js
+++ src/components/views/FriendsView.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
+import Friends from '../Friends/Friends';
 
 export default class FriendsView extends Component {
     render() {
         return (
             <div>
                 <Header TitleOfPage={"Friends"}/> 
-
+                <Friends />
             </div>
         );
     }
