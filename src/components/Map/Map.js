5ecaf5eb882b74b843c23841fdd893b10b333150
Tyler <millert2420@gmail.com>
2018-04-30 13:53:36 -0500

    attempting to fix vulernability issue

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -111,7 +111,7 @@ export default class Map extends Component {
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSrFGEyQytdhP2uxK_GEuYK_Qj1zPnn4c&v=3.exp&libraries=geometry,drawing,places"
 
           loadingElement={<div style={{ height: `100%`,width:`100vw` }} />}
-          containerElement={<div style={{ height: `200px`,width:`100vw` }} />}
+          containerElement={<div style={{ height: `500px`,width:`100vw` }} />}
 
           mapElement={<div style={{ height: `100%` }} />}
           array={this.state.locations}
